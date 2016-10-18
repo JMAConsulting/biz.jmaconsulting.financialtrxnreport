c4d57509e6bdd6faa17e0c9fc5fed0062a5813ac
Joe Murray <joe.murray@jmaconsulting.biz>
2016-10-18 17:46:42 -0400

    changed from fi.amount to ceft.amount in 2 places

--- CRM/Financialtrxnreport/Form/Report/FinancialTransaction.php
+++ CRM/Financialtrxnreport/Form/Report/FinancialTransaction.php
@@ -102,12 +102,12 @@ UNION
       FROM civicrm_financial_trxn cft
       WHERE from_financial_account_id IS NOT NULL
 UNION
-    SELECT cft.id, cfi.id, DATE(cft.trxn_date) AS trxn_date, -cfi.amount total_amount_3, financial_account_id 
+    SELECT cft.id, cfi.id, DATE(cft.trxn_date) AS trxn_date, -ceft.amount total_amount_3, financial_account_id 
       FROM civicrm_financial_item cfi
         INNER JOIN civicrm_entity_financial_trxn ceft ON ceft.entity_id = cfi.id AND ceft.entity_table = 'civicrm_financial_item'
         INNER JOIN civicrm_financial_trxn cft ON cft.id = ceft.financial_trxn_id AND cft.from_financial_account_id IS NULL
 UNION
-    SELECT cft.id, cfi.id, DATE(cft.trxn_date) AS trxn_date, cfi.amount total_amount_4, financial_account_id 
+    SELECT cft.id, cfi.id, DATE(cft.trxn_date) AS trxn_date, ceft.amount total_amount_4, financial_account_id 
       FROM civicrm_financial_item cfi
         INNER JOIN civicrm_entity_financial_trxn ceft ON ceft.entity_id = cfi.id AND ceft.entity_table = 'civicrm_financial_item'
         INNER JOIN civicrm_financial_trxn cft ON cft.id = ceft.financial_trxn_id AND cft.to_financial_account_id IS NULL
